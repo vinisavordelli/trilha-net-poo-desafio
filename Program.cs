2d7dcf1247a6d83530c43b0d8d5c720c48334c6b
Vinicius Savordelli <vinicius.savordelli@gmail.com>
2023-11-30 18:46:39 -0300

    feat(Models & Main): finishes challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Smartphone Nokia:");
+    Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+    nokia.Ligar();
+    nokia.InstalarAplicativo("Whatsapp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+    Console.WriteLine("\n");
+
+    Console.WriteLine("Smartphone Iphone:");
+    Smartphone iphone = new Iphone(numero: "67890", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+    iphone.Ligar();
+    iphone.InstalarAplicativo("Telegram");
+  }
+}
